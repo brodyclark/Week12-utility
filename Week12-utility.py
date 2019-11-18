45d00107d7be68320ac25cc20a8c6d0e33c2f7f6
brodyclark <brodyclark@mymail.mines.edu>
2019-11-18 16:40:22 -0700

    def PrintOutput

--- Week12-utility.py
+++ Week12-utility.py
@@ -3,9 +3,9 @@
 # CSCI102 - Section C
 # Week 12 - Part A
 
-##def PrintOutput(string):
-##     print('OUTPUT', string)
-##
+def PrintOutput(string):
+     print('OUTPUT', string)
+
 ##def LoadFile(file_name):
 ##    with open(file_name, 'r') as read_file:
 ##        lines = read_file.readlines()
