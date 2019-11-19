95dd0e6a4c79361f46416b056e37efbe500e42ae
brodyclark <brodyclark@mymail.mines.edu>
2019-11-18 22:05:42 -0700

    def UpdateString

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,3 +10,17 @@ def LoadFile(file_name):
     with open(file_name, 'r') as read_file:
         lines = read_file.readlines()
     print(lines)
+
+def UpdateString(string_one, string_two, integer):
+     list_one = list(string_one)
+     
+     for index in range(len(list_one)):
+          
+          if index == integer:
+               list_one[index] = string_two
+     
+     print(''.join(list_one))
+
+
+     
+               
