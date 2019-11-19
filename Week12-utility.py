a13cdff57994e4f9b198a14e732d585b4cc6eb10
brodyclark <brodyclark@mymail.mines.edu>
2019-11-18 21:52:27 -0700

    def LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,7 +6,7 @@
 def PrintOutput(string):
      print('OUTPUT', string)
 
-##def LoadFile(file_name):
-##    with open(file_name, 'r') as read_file:
-##        lines = read_file.readlines()
-##    print(lines)
+def LoadFile(file_name):
+    with open(file_name, 'r') as read_file:
+        lines = read_file.readlines()
+    print(lines)
